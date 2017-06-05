945caecf219030dc56f8feab3e0c5d1b61ee28a9
tibi0930 <tibi-macbook@Tibor-Mac-2.local>
2017-06-05 10:48:37 +0200

    Initial Commit

---
+++ Chapter 1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chapter 1
+//
+//  Created by Tibor Dénes Baracskai on 2017. 06. 05..
+//  Copyright © 2017. Tibor Dénes Baracskai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
